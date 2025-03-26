e44d9ffcec8e4b32a42aaa87eeee35626d2637a0
plutonium-exe <nebulastargt@gmail.com>
2025-03-26 17:46:06 +0000

    Created keys

---
+++ src/keys.js
@@ -0,0 +1,14 @@
+{
+  "validKeys": [
+    "G7mX2pZqW9aL8sVfT4dYbC3Nj",
+    "K9vX3pLmQ7aZ2WbT8YfC4dN6J",
+    "A3bT4nX7vY9pZ1kW5dQ2mL6R",
+    "F8sJ7dG1kV0W9rY2zL5bM3QX",
+    "N4oX2pL7mK1zV8wT3Y9fQ6Bd",
+    "J2cQ5kV1zX8mB6Y9aT7W3pL4J",
+    "P1tD8V2yQ7Z6bX5kL9W3fN0aM",
+    "D4yT7mL9V2kX6pJ0bQ8Y1fW3R",
+    "Z1pM9N2tL7X4V5Q8kY3bF0J9W"
+  ]
+}
+// if you are experiencing lag issues try another key for another server
